7a3dbb9ec7d3b5249b7dd8b350ef52aafa39b2a8
水原樹 <>
2025-11-03 18:25:25 +0900

    first commit

---
+++ SpeakingAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeakingAR
+//
+//  Created by 水原樹 on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeakingAR/SpeakingARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakingARApp.swift
+//  SpeakingAR
+//
+//  Created by 水原樹 on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct SpeakingARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
